8b384e80521bf40cc7bd6e50819e279534e90461
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2015-09-30 11:40:13 -0500

    Paso 2: Redis y ActiveJob + multiples containers de app

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ActiveRecord::Base
+  validates :name, presence: true
+  after_create { |person| LogPersonJob.perform_later person }
 end
--- dev-entrypoint.sh
+++ dev-entrypoint.sh
@@ -1,21 +1,32 @@
 #! /bin/bash
 
-# Agregar los binstubs a $PATH:
-export PATH=/usr/src/app/bin:$PATH
-
 set -e
 
-if [ -z "$1" ]; then
+# 1: Agregar los binstubs a $PATH:
+export PATH=/usr/src/app/bin:$PATH
 
-  # 1:
-  bundle check || bundle
+# 2: "Esperar" a que termine el setup del otro container de la app:
+if [ -z "$APP_SETUP_WAIT" ]; then APP_SETUP_WAIT=5; fi
+while [ -f /usr/src/app/tmp/app-setup-is-busy ]
+do
+  echo "Waiting $APP_SETUP_WAIT seconds for another app container to finish setup..."
+  sleep $APP_SETUP_WAIT
+done
 
-  # 2:
-  rake db:migrate || rake db:setup
+# 3: "Apartar el proceso de setup:
+touch /usr/src/app/tmp/app-setup-is-busy
 
-  # 3:
-  set -- rails server -p 3000 -b 0.0.0.0
-fi
+# 4: Revisar o Instalar dependencias de la app:
+bundle check || bundle
 
-# ejecutar el comando o el default:
+# 5: Actualizar o Instalar la base de datos:
+rake db:migrate || rake db:setup
+
+# 6: "Liberar" el proceso de setup:
+rm -rf /usr/src/app/tmp/app-setup-is-busy
+
+# 7: Especificar el comando default:
+if [ -z "$1" ]; then set -- rails server -p 3000 -b 0.0.0.0 "$@"; fi
+
+# 8: Ejecutar el comando o el default:
 exec "$@"
