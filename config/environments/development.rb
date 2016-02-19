70d122ec5fd43d06645b1f22a0beb8eb9f535833
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2016-02-19 13:49:51 -0600

    Fix: Allow rails web console whitelisted IP's to be configured from the compose dotenv file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Configure access to the web console from
+  config.web_console.whitelisted_ips = ENV["WEB_CONSOLE_WHITELISTED_IPS"].split \
+    if ENV.key? "WEB_CONSOLE_WHITELISTED_IPS"
 end
