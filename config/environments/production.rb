9c679fc493da65d742cbcf82367bdb272a5589fa
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2016-02-22 18:12:56 -0600

    Enabled logging to STDOUT for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+  config.logger = ActiveSupport::TaggedLogging.new ::Logger.new(STDOUT)
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
